bc153143da40efff588deb9c890bee6f33fccdfa
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-10-11 19:54:38 -0300

    acrescentando codigos

--- aulas_laravel/app/Http/Controllers/Main.php
+++ aulas_laravel/app/Http/Controllers/Main.php
@@ -17,11 +17,44 @@ class Main extends Controller
     // public function mostrarNome(){
     //      return view('pagina2',['nome' => 'Juliane Bezerra']);
     //     }
-    public function user($nome, $apelido){
+    // public function user($nome, $apelido){
         
-        return view('user', [
-            'nome' => $nome,
-            'apelido' => $apelido
-        ]);
+    //     return view('user', [
+    //         'nome' => $nome,
+    //         'apelido' => $apelido
+    //     ]);
+    // }
+
+    // public function nome($nome, $idade,  $apelido = ''){
+        
+    //     return view('nome', [
+    //         'nome' => $nome,
+    //         'apelido' => $apelido,
+    //         'idade' => $idade
+    //     ]);
+    // }
+
+    public function home(){
+        echo'<h3>HOME<h3>';
+        echo '<hr>';
+        echo '<a href="'.route('inicio').'">Home</b><br>';
+        echo '<a href="'.route('servicos').'">Servicos</b><br>';
+        echo '<a href="'.route('contatos').'">Contatos</b><br>';
+    }
+
+    public function servicos(){
+        echo'<h3>SERVICOS<h3>';
+        echo '<hr>';
+        echo '<a href="'.route('inicio').'">Home</b><br>';
+        echo '<a href="'.route('servicos').'">Servicos</b><br>';
+        echo '<a href="'.route('contatos').'">Contatos</b><br>';
+    }
+
+    public function contatos(){
+        echo'<h3>CONTATOS<h3>';
+        echo '<hr>';
+        echo '<a href="'.route('inicio').'">Home</b><br>';
+        echo '<a href="'.route('servicos').'">Servicos</b><br>';
+        echo '<a href="'.route('contatos').'">Contatos</b><br>';
     }
 }
